1e5bee34fc32ab14316b9da3fe6fc7efbd4d8a6f
Chen <a675750333@gmail.com>
2021-01-31 01:55:36 +0800

    切换至zookeeper

--- src/main/java/com/wantocode/clientopenfeign/api/OpenFeignClient.java
+++ src/main/java/com/wantocode/clientopenfeign/api/OpenFeignClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
  * @author Chen
  * @date 2021/1/30
  */
-@FeignClient("service-info-cluster")
+@FeignClient("service-info")
 public interface OpenFeignClient {
     @GetMapping("/getInfo")
     public String getInfo();
